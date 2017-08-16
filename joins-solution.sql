eba357732446239dd46044ce472fc6058c9a6878
Ami Schroeder <arschroeder22@gmail.com>
2017-08-16 14:55:22 -0500

    finish 3 & 4

--- joins-solution.sql
+++ joins-solution.sql
@@ -7,7 +7,20 @@ SELECT * FROM orders
 JOIN line_items ON orders.id = line_items.order_id;
 
 -- 3. Which warehouses have cheetos?
+SELECT * FROM warehouse
+JOIN warehouse_product 
+ON warehouse.id=warehouse_product.warehouse_id
+JOIN products 
+ON products.id=warehouse_product.product_id
+where description='cheetos';
+
 -- 4. Which warehouses have diet pepsi?
+SELECT * FROM warehouse
+JOIN warehouse_product 
+ON warehouse.id=warehouse_product.warehouse_id
+JOIN products 
+ON products.id=warehouse_product.product_id
+where description='diet pepsi';
 -- 5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
 -- 6. How many customers do we have?
 -- 7. How many products do we carry?
